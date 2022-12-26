eb27b4b4dc5911c5435fa91d8e04779d1fa16e32
potter-Zhang <90118823+potter-Zhang@users.noreply.github.com>
2022-12-26 19:16:36 +0800

    Add files via upload

--- NeuralNetwork/BPNNF.m
+++ NeuralNetwork/BPNNF.m
@@ -1,77 +1,178 @@
-function [thetaVec, biasVec] = BPNNF(x, y, layer, alpha, count)
+function [thetaVec, biasVec] = BPNNF(x, y, layer, lambda, alpha, count, option, oldThetaVec, oldBiasVec)
+% BPNNF: implements neural network
 % x is a m * Q matrix
 % y is a n * Q matrix 
-% where Q is number of samples
+% where Q is number of samples, and m, n are dimentions of input and output
+% layer is a struct, layer.num is number of hidden layers, layer.units
+% specify numbers of neurons in each hidden layer, respectively
+% lambda is the coefficient of regulization term
+% alpha is learning rate
+% count is number of iteration
+% option is about adaptive learning rate algorithms, option.method choose
+% methods and option.cof give the corresponding coefficients
+% oldThetaVec and oleBiasVec are the parameter you want to train again
+% return thetaVec and biasVec, which are weight matrix and bias parameters
 
-f = @tansig;
-g = @transigDer;
+% activation function and its derivative function
+f = @ReLU;
+g = @ReLUDer;
+h = @simple;
+
+% adaptive learning rate algorithms options
+if (nargin >= 7)
+switch(option.method)
+    case 'AdaGrad'
+        h = @AdaGrad;
+    case 'RMSProp'
+        h = @RMSProp;
+    case 'Adam'
+        h = @Adam;
+
+
+    
+end
+else
+    option.cof = 0;
+end
 
 % normalization
-x=2 * (x-min(x, [], 2))./(max(x, [], 2)-min(x, [], 2)) - ones(size(x));
-%tmpy = y;
-y=2 * (y-min(y, [], 2))./(max(y, [], 2)-min(y, [], 2)) - ones(size(y));
 
+%x = range * (x-min(x, [], 2))./(max(x, [], 2)-min(x, [], 2)) - (range / 2) * ones(size(x));
+
+%y = range * (y-min(y, [], 2))./(max(y, [], 2)-min(y, [], 2)) - (range / 2) * ones(size(y));
+%y = y .* 100;
+% add input and output layers
 layer.num = layer.num + 2;
 layer.units = [size(x, 1), layer.units, size(y, 1)];
 
+
 % initialization
+originalX = x;
+originalY = y;
+seq = 1 : (size(x, 2) / 50);
 a = containers.Map('KeyType', 'int32', 'ValueType', 'any');
-z = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+z = containers.Map('KeyType', 'int32', 'ValueType', 'any'); 
 thetaVec = containers.Map('KeyType', 'int32', 'ValueType', 'any');
-biasVec = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+biasVec = containers.Map('KeyType', 'int32', 'ValueType', 'any');  
 thetaGrad = containers.Map('KeyType', 'int32', 'ValueType', 'any');
 biasGrad = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+r = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+s = containers.Map('KeyType', 'int32', 'ValueType', 'any');
 
 for i = 1 : layer.num - 1
     thetaVec(i) = rand(layer.units(i + 1), layer.units(i));
     biasVec(i) = rand(layer.units(i + 1), 1);
     thetaGrad(i) = zeros(size(thetaVec(i)));
     biasGrad(i) = zeros(size(biasVec(i)));
+    r(i) = zeros(size(thetaGrad(i)));
+    r(i + layer.num - 1) = zeros(size(biasGrad(i)));
+    s(i) = zeros(size(thetaGrad(i)));
+    s(i + layer.num - 1) = zeros(size(biasGrad(i)));
+end
+
+if nargin == 9
+    thetaVec = oldThetaVec;
+    biasVec = oldBiasVec;
 end
 
 % number of samples
 Q = size(x, 2);
+volume = Q;
 
 for iteration = 1 : count
+x = originalX(:, seq);
+y = originalY(:, seq);
+Q = size(x, 2);
+if (mod(iteration, 10) == 0)
+seq = mod(seq + length(seq) - 1, volume) + 1;
+end
+
 
 % forward
 a(1) = x;
 for i = 1 : layer.num - 1
     z(i + 1) = thetaVec(i) * a(i) + biasVec(i);
-    a(i + 1) = f(z(i + 1));
+    %disp(z(i + 1))
+    if i ~= layer.num - 1
+        a(i + 1) = f(z(i + 1));
+    else 
+        a(i + 1) = net(z(i + 1));
+    end
 end
 
-% backward
+% predicted values
 afinal = a(layer.num);
+disp("准确率为："+test(afinal, y, 0));
 
-disp('cost = ');
-disp(sum(sum((afinal - y) .^ 2)));
-%y=2*(y-min(y, [], 2))./(max(y, [], 2)-min(y, [], 2))-ones(size(y));
-%disp('ytp = ');
-%disp(afinal);
-%disp((afinal + ones(size(afinal))).* (max(tmpy, [], 2) - min(tmpy, [], 2)) ./ 2 + min(tmpy, [], 2));
+% display cost
+%disp('cost = ');
+%disp(sum(sum((-y .* log(afinal)))) / Q);
+%plot(iteration, sum(sum((afinal - y) .^ 2)), 'r*');
+%hold on;
 
+% bcak propagation
 for i = 1 : size(x, 2)
-    cost = afinal(:, i) - y(:, i);
+    cost = (-y(:, i) + afinal(:, i));%afinal(:, i) - y(:, i) ;
     for j = layer.num - 1 : -1 : 1
         tmpa = a(j + 1);
         tmpz = z(j + 1);
-        fDer = g(tmpa(:, i), tmpz(:, i));
+        fDer = g(tmpa(:, i), tmpz(:, i));      
         tmpa = a(j);
-        thetaGrad(j) = thetaGrad(j) + (cost .* fDer) * tmpa(:, i)' / Q;
-        biasGrad(j) = biasGrad(j) + (cost .* fDer) / Q;
-        cost =  thetaVec(j)' * (cost .* fDer);
+        if j == layer.num - 1
+            thetaGrad(j) = thetaGrad(j) + cost * tmpa(:, i)' / Q;
+            biasGrad(j) = biasGrad(j) + cost / Q;       
+        else
+            thetaGrad(j) = thetaGrad(j) + (cost .* fDer) * tmpa(:, i)' / Q;
+            biasGrad(j) = biasGrad(j) + (cost .* fDer) / Q;
+        end
+        if j == layer.num - 1
+            cost =  thetaVec(j)' * cost;
+        else
+            cost =  thetaVec(j)' * (cost .* fDer);
+        end
+        
     end
 end
 
 % gradient descent
 for i = 1 : layer.num - 1
-    thetaVec(i) = thetaVec(i) - alpha * thetaGrad(i);
-    biasVec(i) = biasVec(i) - alpha * biasGrad(i);
+    [thetaGrad(i), r(i), s(i)] = h(thetaGrad(i), alpha, r(i), s(i), option.cof, iteration);
+    [biasGrad(i), r(i + layer.num - 1), s(i + layer.num - 1)] = h(biasGrad(i), alpha, r(i + layer.num - 1), s(i + layer.num - 1), option.cof, iteration);
+    thetaVec(i) = thetaVec(i) + thetaGrad(i) + lambda * thetaVec(i) / Q;
+    biasVec(i) = biasVec(i) + biasGrad(i) + lambda * biasVec(i) / Q;
     thetaGrad(i) = zeros(size(thetaGrad(i)));
     biasGrad(i) = zeros(size(biasGrad(i)));
 end
+end
+end
 
+function [deltaGrad, newR, newS]  = simple(g, alpha, r, s, ~, ~)
+    deltaGrad = -alpha * g;
+    newR = r;
+    newS = s;
 end
+
+function [deltaGrad, newR, newS] = AdaGrad(g, alpha, r, s, ~, ~) 
+    newR = r + g.^2;
+    deltaGrad = -(alpha ./ (1e-7 + sqrt(newR))) .* g;  
+    newS = s;
 end
+
+function [deltaGrad, newR, newS] = RMSProp(g, alpha, r, s, p, ~)
+    newR = p * r + (1 - p) * g.^2;
+    deltaGrad = -(alpha ./ sqrt(1e-6 + newR)) .* g;   
+    newS = s;
+end
+
+function [deltaGrad, newR, newS] = Adam(g, alpha, r, s, cof, t)
+    newS = cof(1) * s + (1 - cof(1)) * g;
+    newR = cof(2) * r + (1 - cof(2)) * g.^2;
+    sEst = newS / (1 - cof(1).^t);
+    rEst = newR / (1 - cof(2).^t);
+    deltaGrad = -alpha * (sEst ./ (sqrt(rEst) + 1e-8));
+    
+end
+
+
+
 
