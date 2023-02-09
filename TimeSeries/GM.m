84a5f5003fc11cd11684409e576e38a998b21ddf
potter-Zhang <90118823+potter-Zhang@users.noreply.github.com>
2023-02-09 18:07:20 +0800

    添加了检验，修正了错误

--- TimeSeries/GM.m
+++ TimeSeries/GM.m
@@ -1,9 +1,11 @@
 function [P, f] = GM(x)
-% x的每一行是一个样本
+% each row of x is a sample
+% x should be m * 1
 [m, n] = size(x);
 
 % Y
 x0 = x(1, :);
+originalX = x;
 Y = x(2 : end, :);
 
 % add up
@@ -24,6 +26,43 @@ B = [-z ones(m - 1, 1)];
 P = (B' * B) \ B' * Y;
 
 % function
+% f = @(t) (1 - exp(P(1))) .* (x0 - P(2) / P(1)) .* exp(-P(1) * (t - 1));
+g = @(t) (x0 - P(2) / P(1)) .* exp(-P(1) .* (t - 1)) + P(2) / P(1);
+f = @(t) recover(g(t));
 
-f = @(t) (1 - exp(P(1))) .* (x0 - P(2) / P(1)) .* exp(-P(1) * (t - 1));
+% testing
+t = 1 : m;
+t = t';
+xtp = f(t);
+
+if n == 1
+    S1 = sqrt(sum((originalX - xtp) .^ 2) / m);
+    S2 = sqrt(sum((originalX - mean(originalX)) .^ 2) / m);
+else
+    S1 = sqrt(sum(sum((originalX - xtp) .^ 2)) / m);
+    S2 = sqrt(sum(sum((originalX - mean(originalX))) .^ 2) / m);
+end
+C = S1 / S2;
+
+disp('C = ');
+disp(C);
+
+if C < 0.35
+    disp('优');
+elseif C  < 0.5
+    disp('合格');
+elseif C < 0.65
+    disp('勉强合格');
+else 
+    disp('不合格');
+end
+    
+end
+
+function [y] = recover(x)
+    y = zeros(size(x));
+    y(1) = x(1);
+    for i = 2 : length(x)
+        y(i) = x(i) - x(i - 1);
+    end
 end
